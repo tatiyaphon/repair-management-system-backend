0927899363f70469f57d766bd5a65e93a234ac93
tatiyaphon <tatiyaphon.bo@rmuti.ac.th>
2026-02-03 18:27:36 +0700

    add some features

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -421,6 +421,7 @@ tbody td{
       <div class="row"><div class="label">ชื่อลูกค้า</div>${job.customerName}</div>
       <div class="row"><div class="label">เบอร์โทร</div>${job.customerPhone || "-"}</div>
       <div class="row"><div class="label">ที่อยู่</div>${job.customerAddress || "-"}</div>
+      <div class="row"><div class="label">อุปกรณ์ที่มาด้วย</div>${job.accessories || "-"}</div>
     </div>
 
     <div class="box">
