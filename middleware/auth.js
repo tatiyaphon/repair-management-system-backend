1e79dd366a11bc81c3deeb212b1c7ea11ad90b21
tatiyaphon <tatiyaphon.bo@rmuti.ac.th>
2026-02-03 12:14:44 +0700

    add some features

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,22 @@
-const jwt = require("jsonwebtoken");
-
 module.exports = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+
+  const header = req.headers.authorization;
+  if (!header) {
     return res.status(401).json({ message: "No token" });
   }
 
-  const token = req.headers.authorization?.split(" ")[1];
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = {
-      userId: decoded.id,
-      role: decoded.role
-    };
-    next();
-  } catch (err) {
-    res.status(401).json({ message: "Invalid token" });
+  const token = header.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Invalid token format" });
   }
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+  req.user = {
+    id: decoded.id,
+    role: decoded.role
+  };
+
+  next();
 };
+
