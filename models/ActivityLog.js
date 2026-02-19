3b5246ae4a56bcc916acf4dcee192c13b9c2966c
tatiyaphon <tatiyaphon.bo@rmuti.ac.th>
2026-02-19 20:27:40 +0700

    add some features

--- models/ActivityLog.js
+++ models/ActivityLog.js
@@ -1,34 +1,23 @@
 const mongoose = require("mongoose");
 
 const activitySchema = new mongoose.Schema({
-
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Employee",
-    required: true
-  },
-
-  userName: {
-    type: String,
-    required: true
-  },
-
   action: {
     type: String,
     required: true
   },
-
-  detail: {
-    type: String,
-    default: "-"
-  },
-
-  jobId: {
+  user: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "Job",
-    default: null
+    ref: "Employee"
+  },
+  job: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Job"
+  },
+  description: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
   }
-
-}, { timestamps: true });
+});
 
 module.exports = mongoose.model("Activity", activitySchema);
