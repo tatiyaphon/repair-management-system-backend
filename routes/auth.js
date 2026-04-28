af44f8e7f92bc1984af4fc5395c8d09d0660103f
tatiyaphon <tatiyaphon.bo@rmuti.ac.th>
2026-04-29 00:29:50 +0700

    add some features

--- routes/auth.js
+++ routes/auth.js
@@ -258,6 +258,16 @@ router.post("/reset-password/:token", async (req, res) => {
   }
 });
 
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
+
 console.log("EMAIL_USER:", process.env.EMAIL_USER);
 console.log("EMAIL_PASS:", process.env.EMAIL_PASS ? "OK" : "MISSING");
 
@@ -266,16 +276,6 @@ transporter.verify(function (error, success) {
     console.log("❌ SMTP ERROR:", error);
   } else {
     console.log("✅ SMTP READY");
-  }
-});
-
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
   }
 });
 
