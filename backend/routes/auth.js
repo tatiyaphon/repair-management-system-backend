e16e3a4b59cb6625b5ad3a64a01ac63287daaef4
tatiyaphon <tatiyaphon.bo@rmuti.ac.th>
2026-07-24 15:46:16 +0700

    add some features

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -12,12 +12,12 @@ const requireRole = require("../middleware/requireRole");
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
-    user: process.env.BREVO_USER,
-    pass: process.env.BREVO_SMTP_KEY,
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
 });
 /* =========================
