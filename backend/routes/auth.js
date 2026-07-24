e50a0f7e80731ed57edef2e38d5ad866dd0138fd
tatiyaphon <tatiyaphon.bo@rmuti.ac.th>
2026-07-24 15:57:03 +0700

    add some features

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -12,9 +12,7 @@ const requireRole = require("../middleware/requireRole");
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
