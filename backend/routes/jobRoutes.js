9b7515164e0e2fdad41d4671aa346eafcf1efb20
tatiyaphon <tatiyaphon.bo@rmuti.ac.th>
2026-07-31 11:57:54 +0700

    add some features

--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -254,6 +254,11 @@ router.put("/:id", auth, async (req, res) => {
       return res.status(404).json({ message: "ไม่พบงานซ่อม" });
     }
 
+    if (job.status === "ซ่อมเสร็จ" || job.status === "ยกเลิก") {
+    return res.status(400).json({
+        message: "งานนี้ถูกปิดแล้ว ไม่สามารถแก้ไขได้"
+    });
+}
     // 🔒 ตรวจสิทธิ์
     if (
       req.user.role !== "admin" &&
